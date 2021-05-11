e477e601924ae4b46b17a60b080b05c5e6828369
Luca Modenese <luca.modenese@gmail.com>
2021-05-11 12:18:28 +0100

    upd based on Forum post (Ayman suggestion)

--- MATLAB_tool/MuscleParOptTool/getOpenSimVersion.m
+++ MATLAB_tool/MuscleParOptTool/getOpenSimVersion.m
@@ -3,31 +3,34 @@
 %    Author:   Luca Modenese,  2021                                       %
 %    email:    l.modenese@imperial.ac.uk                                  %
 % ----------------------------------------------------------------------- %
-% This script returns the OpenSim version for which the environmental
-% variable OPENSIM_HOME has been set.
+% This script returns the OpenSim version
 % ----------------------------------------------------------------------- %
 function [osim_version_float, osim_version_string] = getOpenSimVersion()
 
-% read env variable
-osimpath = getenv('OPENSIM_HOME');
+import org.opensim.modeling.*
 
-% let's assume it's the latest version in case there is no path variable
-if isempty(osimpath)
-    osim_version_float = 4.1;
-    osim_version_string = '4.1';
+% read env variable
+try
+    % method available only from 4.x
+    os_version = char(opensimCommonJNI.GetVersion());
+    
+    % get the field separators, e.g. '-
+    sep_set = strfind(os_version, '-');
+    
+    % use the last file separator to get the OpenSim installation folder name
+    version = os_version(1:sep_set(1));
+    
+    % get the string for the opensim version
+    osim_version_string = strtrim(strrep(lower(version),'opensim', ''));
+    
+    % transform in float
+    osim_version_float = str2double(osim_version_string);
+    
+catch
+    
+    % GetVersion is not available in earlier OpenSim versions
+    osim_version_float = 3.3;
+    osim_version_string = '3.3';
     return
 end
-
-% get the file separators, e.g. '\' in 'C:\Program files\OpenSim 4.1'
-sep_set = strfind(osimpath, filesep);
-
-% use the last file separator to get the OpenSim installation folder name
-version = osimpath(sep_set(end)+1:end);
-
-% get the string for the opensim version
-osim_version_string = strtrim(strrep(lower(version),'opensim', ''));
-
-% transform in float
-osim_version_float = str2num(osim_version_string);
-
 end
